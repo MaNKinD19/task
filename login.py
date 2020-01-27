859288e794bb71e42b1e27f1e764adaedb3fe4c6
MaNKinD19 <59164643+MaNKinD19@users.noreply.github.com>
2020-01-28 02:08:15 +0530

    Update login.py

--- login.py
+++ login.py
@@ -3,6 +3,7 @@ import random
 name=''
 mid=''
 d={}
+det={}
 def reg():
     c=0
     acc=list(range(1000,9999))
@@ -21,17 +22,32 @@ def reg():
     a=acc.pop()
     print("Account number :",a)
     pwd=getpass.getpass("Enter  password :")
+    c=0
+    while c<3:
+        bal=float(input("Enter balance amount(>5000) "))
+        if bal<5000:
+            print("No ,minimum balance")
+            c=c+1
+        else:
+            print("Amount credited succesfully!!!")
+            break
     d.update({a:pwd})
+    det.update({a:[name,mid,bal,pwd]})
     print("Account registeed succesfully!!!")
 reg()
+def dep():
+    
 def login():
-    uno=input("Enter Account number ")
-    upwd=getpass.getpass("Enter password")
-    if uno in d and upwd == d[uno]:
-        print("Login succesfully")
-        
-    else:
-        print("Try again")
+    c1=0
+    while c1<3:
+        uno=int(input("Enter Account number "))
+        upwd=getpass.getpass("Enter password ")
+        if uno in d and upwd == d[uno]:
+            print("Login succesfully")
+            break
+        else:
+            print("Try again")
+            c1=c1+1
 login()
 
-    
+
