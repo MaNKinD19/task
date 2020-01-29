5287b59e7b1516dad1f092da798e6d97020031e2
MaNKinD19 <59164643+MaNKinD19@users.noreply.github.com>
2020-01-29 19:38:47 +0530

    bank.py

--- login.py
+++ login.py
@@ -38,14 +38,48 @@ def reg():
     print("Account registeed succesfully!!!")
     f.close()
 reg()
-def dep():
+
+def dep(userid):
     f.open("Bank.txt","r+")
     u=eval(f.read())
     damt=float(input("Enter Amount"))
+    if damt<500 and damt>50000:
+        print("Limit exceeded")
+    else:
+        print("Deposited succesfully")
+        bal=bal+damt
+    f=open("Bank.txt","w+")
+    u[userid][2]=bal    
+    f.write("%s\r\n"%u)
+    f.close()
     
+def wdraw(userid):
+    f.open("Bank.txt","r+")
+    v=eval(f.read())
+    wamt=float(input("Enter Amount"))
+    if wamt<500 and wamt>50000:
+        print("Limit exceeded")
+    elif (bal-wamt)<5000:
+        print("exceeding Minimum Balance")
+    else:
+        print("Withdrawl succesful")
+        bal=bal-wamt
+    f=open("Bank.txt","w+")
+    u[userid][2]=bal    
+    f.write("%s\r\n"%u)
+    f.close()
+    
+def bcheck(userid):
+    f.open("Bank.txt","r+")
+    w=eval(f.read())
+    bal=r[userid][2]
+    print("Balance :",bal)
+    f.close()
     
 def login():
     c1=0
+    f=open("Bank.txt","r+")
+    x=eval(f.read())
     while c1<3:
         uno=int(input("Enter Account number "))
         upwd=getpass.getpass("Enter password ")
@@ -55,4 +89,8 @@ def login():
         else:
             print("Try again")
             c1=c1+1
+    ch=int(input("""Enter your choice 1. Deposit
+    2. Withdraw
+    3. Check Balance"""))
+    
 login()
